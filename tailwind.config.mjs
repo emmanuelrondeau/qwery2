79d4d0e4bea1413c6f94ee51fee65d3c7878f810
Fio Cobbs <20820404-fiocobbs@users.noreply.gitlab.com>
2024-04-22 20:21:27 -0500

    feat: initial markdoc implementation
    Includes article edits.

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -2,7 +2,7 @@
 const defaultTheme = require("tailwindcss/defaultTheme");
 
 export default {
-	content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+	content: ["./src/**/*.{astro,html,js,jsx,md,mdx,mdoc,svelte,ts,tsx,vue}"],
 	theme: {
 		fontFamily: {
 			"josefin-sans": ['"Josefin Sans"'],
