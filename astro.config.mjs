623ac51517861b6f4b8c04e30695918b666d850a
Fio Cobbs <20820404-fiocobbs@users.noreply.gitlab.com>
2024-05-21 23:39:23 -0500

    feat: switch to decap cms

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,15 +1,33 @@
 import { defineConfig } from "astro/config";
+import AutoImport from "astro-auto-import";
 import react from "@astrojs/react";
 import tailwind from "@astrojs/tailwind";
 import mdx from "@astrojs/mdx";
-import { remarkReadingTime } from "./remark-reading-time.mjs";
 
-import markdoc from "@astrojs/markdoc";
+import { remarkReadingTime } from "./remark-reading-time.mjs";
 
 // https://astro.build/config
 export default defineConfig({
-  markdown: {
-    remarkPlugins: [remarkReadingTime]
-  },
-  integrations: [react(), tailwind(), mdx(), markdoc()]
+	markdown: {
+		remarkPlugins: [remarkReadingTime],
+	},
+	integrations: [
+		AutoImport({
+			// https://github.com/delucis/astro-auto-import/tree/main/packages/astro-auto-import
+			imports: [
+				"/src/components/primitives/Callout.astro",
+				"/src/components/articles/Image.astro",
+				"/src/components/articles/FigureCaption.astro",
+				{
+					"/src/assets/images/default-thumb.jpg": [
+						["default", "DefaultImageCover"],
+					],
+				},
+			],
+		}),
+		react(),
+		tailwind(),
+		mdx(),
+	],
+	output: "hybrid",
 });
