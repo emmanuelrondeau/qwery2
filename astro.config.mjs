a8b3fa0ec2dfc0b14582915ab4afe3ad0555919d
Fio Cobbs <20820404-fiocobbs@users.noreply.gitlab.com>
2024-05-22 02:08:52 -0500

    feat: seo descriptions
    Includes remark a11y emoji plugin.

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,21 +3,29 @@ import AutoImport from "astro-auto-import";
 import react from "@astrojs/react";
 import tailwind from "@astrojs/tailwind";
 import mdx from "@astrojs/mdx";
+import a11yEmoji from "@fec/remark-a11y-emoji";
 import { remarkReadingTime } from "./remark-reading-time.mjs";
 
 import netlify from "@astrojs/netlify";
 
 // https://astro.build/config
 export default defineConfig({
-  markdown: {
-    remarkPlugins: [remarkReadingTime]
-  },
-  integrations: [AutoImport({
-    // https://github.com/delucis/astro-auto-import/tree/main/packages/astro-auto-import
-    imports: ["/src/components/primitives/Callout.astro", "/src/components/articles/Image.astro", "/src/components/articles/FigureCaption.astro", {
-      "/src/assets/images/default-thumb.jpg": [["default", "DefaultImageCover"]]
-    }]
-  }), react(), tailwind(), mdx()],
-  output: "hybrid",
-  adapter: netlify()
+	markdown: {
+		remarkPlugins: [remarkReadingTime, a11yEmoji],
+	},
+	integrations: [
+		AutoImport({
+			// https://github.com/delucis/astro-auto-import/tree/main/packages/astro-auto-import
+			imports: [
+				"/src/components/primitives/Callout.astro",
+				"/src/components/articles/Image.astro",
+				"/src/components/articles/FigureCaption.astro",
+			],
+		}),
+		react(),
+		tailwind(),
+		mdx(),
+	],
+	output: "hybrid",
+	adapter: netlify(),
 });
