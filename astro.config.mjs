64abf0063627c6307e732e17f15d73c30b5f7f76
Fio Cobbs <20820404-fiocobbs@users.noreply.gitlab.com>
2024-05-22 00:31:55 -0500

    fix: netlify adapter deployment

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,31 +3,21 @@ import AutoImport from "astro-auto-import";
 import react from "@astrojs/react";
 import tailwind from "@astrojs/tailwind";
 import mdx from "@astrojs/mdx";
-
 import { remarkReadingTime } from "./remark-reading-time.mjs";
+
+import netlify from "@astrojs/netlify";
 
 // https://astro.build/config
 export default defineConfig({
-	markdown: {
-		remarkPlugins: [remarkReadingTime],
-	},
-	integrations: [
-		AutoImport({
-			// https://github.com/delucis/astro-auto-import/tree/main/packages/astro-auto-import
-			imports: [
-				"/src/components/primitives/Callout.astro",
-				"/src/components/articles/Image.astro",
-				"/src/components/articles/FigureCaption.astro",
-				{
-					"/src/assets/images/default-thumb.jpg": [
-						["default", "DefaultImageCover"],
-					],
-				},
-			],
-		}),
-		react(),
-		tailwind(),
-		mdx(),
-	],
-	output: "hybrid",
+  markdown: {
+    remarkPlugins: [remarkReadingTime]
+  },
+  integrations: [AutoImport({
+    // https://github.com/delucis/astro-auto-import/tree/main/packages/astro-auto-import
+    imports: ["/src/components/primitives/Callout.astro", "/src/components/articles/Image.astro", "/src/components/articles/FigureCaption.astro", {
+      "/src/assets/images/default-thumb.jpg": [["default", "DefaultImageCover"]]
+    }]
+  }), react(), tailwind(), mdx()],
+  output: "hybrid",
+  adapter: netlify()
 });
