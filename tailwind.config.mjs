9eddf085904565ee7e6a61af39ccbe4b29c3b663
Fio Cobbs <20820404-fiocobbs@users.noreply.gitlab.com>
2024-05-24 17:05:06 -0500

    fix: accessibility tweaks

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -5,7 +5,16 @@ export default {
 	content: ["./src/**/*.{astro,html,js,jsx,md,mdx,mdoc,svelte,ts,tsx,vue}"],
 	theme: {
 		fontFamily: {
-			"josefin-sans": ['"Josefin Sans"'],
+			"josefin-sans": [
+				'"Josefin Sans"',
+				"ui-sans-serif",
+				"system-ui",
+				"sans-serif",
+				"Apple Color Emoji",
+				"Segoe UI Emoji",
+				"Segoe UI Symbol",
+				"Noto Color Emoji",
+			],
 			mono: [
 				"ui-monospace",
 				"SFMono-Regular",
