570645364a74e77ee9684ef444859366db613ccd
Fio Cobbs <20820404-fiocobbs@users.noreply.gitlab.com>
2024-06-23 23:53:06 -0500

    chore: fix types
    No clue how these refactors work, but they fixed something I think?

--- src/env.d.ts
+++ src/env.d.ts
@@ -5,3 +5,11 @@
 declare var StarlightThemeProvider: {
 	updatePickers(theme?: string): void;
 };
+
+declare module "*.mdx" {
+	import type { MDXProps } from "mdx/types";
+	import type { Toc } from "@stefanprobst/rehype-extract-toc";
+
+	export const tableOfContents: Toc;
+	export default function MDXContent(props: MDXProps): JSX.Element;
+}
