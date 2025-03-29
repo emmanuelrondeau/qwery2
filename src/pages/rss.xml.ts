44e6afafdad25aeff1402b4061b0098c149d49a2
Fio Cobbs <20820404-fiocobbs@users.noreply.gitlab.com>
2025-03-28 21:39:45 -0500

    chore: formatting

--- src/pages/rss.xml.ts
+++ src/pages/rss.xml.ts
@@ -3,7 +3,7 @@ export const prerender = true;
 import rss from "@astrojs/rss";
 import { getCollection } from "astro:content";
 
-export async function GET(context: { site: string; }) {
+export async function GET(context: { site: string }) {
 	const posts = await getCollection("posts");
 	return rss({
 		// `<title>` field in output xml
