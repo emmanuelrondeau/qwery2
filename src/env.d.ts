c9b9c587ff60452b22d8aeef2be40458b2e99967
Fio Cobbs <20820404-fiocobbs@users.noreply.gitlab.com>
2025-04-05 21:30:16 -0500

    refactor: change var to let
    Done likely with bad eslint config, so it complained. Doesn't matter much at least

--- src/env.d.ts
+++ src/env.d.ts
@@ -2,7 +2,7 @@
 /// <reference types="astro/client" />
 
 // https://github.com/withastro/starlight/blob/d8afbc221c8975e7dfba916d244a3feaa7ab63e9/packages/starlight/global.d.ts#L3
-declare var StarlightThemeProvider: {
+declare let StarlightThemeProvider: {
 	updatePickers(theme?: string): void;
 };
 
