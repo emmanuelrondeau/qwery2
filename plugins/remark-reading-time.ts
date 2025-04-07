2956df5399d5b2d50362ab64e8c7677f90a651bf
Fio Cobbs <20820404-fiocobbs@users.noreply.gitlab.com>
2025-04-06 20:03:35 -0500

    refactor: abide by formatting linting rules for plugins
    Includes package.json command edit to include plugins folder

--- plugins/remark-reading-time.ts
+++ plugins/remark-reading-time.ts
@@ -1,10 +1,11 @@
-import getReadingTime from "reading-time";
-import { toString } from "mdast-util-to-string";
 import type { Root } from "hast";
+import { toString as hastToString } from "mdast-util-to-string";
+import getReadingTime from "reading-time";
 
 export default function remarkReadingTime() {
-	return function (tree: Root, { data }: any) {
-		const textOnPage = toString(tree);
+	// biome-ignore lint/suspicious/noExplicitAny: hast types are a fuck
+	return (tree: Root, { data }: any) => {
+		const textOnPage = hastToString(tree);
 		const readingTime = getReadingTime(textOnPage);
 		// readingTime.text will give us minutes read as a friendly string,
 		// i.e. "3 min read"
