72ee29b9508ea442d342722127bd5572f0dce7a7
Fio Cobbs <20820404-fiocobbs@users.noreply.gitlab.com>
2025-04-07 23:50:12 -0500

    feat: replace "#" character with link icon

--- plugins/rehype-autolink.ts
+++ plugins/rehype-autolink.ts
@@ -8,7 +8,16 @@ import rehypeAutolinkHeadings, {
 const AnchorLinkIcon = h(
 	"span",
 	{ ariaHidden: "true", class: "anchor-icon" },
-	"#",
+	// "#",
+	h(
+		"svg",
+		{ width: 24, height: 24, viewBox: "0 -960 960 960" },
+		// https://fonts.google.com/icons?selected=Material%20Symbols%20Outlined%3Alink%3AFILL%400%3Bwght%40400%3BGRAD%400%3Bopsz%4024&icon.query=link&icon.size=24&icon.color=inherit
+		h("path", {
+			fill: "currentcolor",
+			d: "M440-280H280q-83 0-141.5-58.5T80-480q0-83 58.5-141.5T280-680h160v80H280q-50 0-85 35t-35 85q0 50 35 85t85 35h160v80ZM320-440v-80h320v80H320Zm200 160v-80h160q50 0 85-35t35-85q0-50-35-85t-85-35H520v-80h160q83 0 141.5 58.5T880-480q0 83-58.5 141.5T680-280H520Z",
+		}),
+	),
 );
 
 /**
