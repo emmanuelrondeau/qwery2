347145db5f682cda578a69ef5f78050950ac777a
Fio Cobbs <20820404-fiocobbs@users.noreply.gitlab.com>
2025-04-25 22:06:49 -0500

    fix: set actual theme within dropdown
    includes removing unused export

--- src/stores.ts
+++ src/stores.ts
@@ -3,7 +3,3 @@ import { writable } from "svelte/store";
 export type Theme = "auto" | "dark" | "light";
 
 export const theme = writable("auto" as Theme);
-export const selectedTheme = writable({
-	value: "auto" as Theme,
-	label: "Auto",
-});
