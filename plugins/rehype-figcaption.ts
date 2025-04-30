05b67d5b7d66a93bca0d179cbb055213ef46a259
Fio Cobbs <20820404-fiocobbs@users.noreply.gitlab.com>
2025-04-30 02:46:33 -0500

    feat: improve figure accessibility

--- plugins/rehype-figcaption.ts
+++ plugins/rehype-figcaption.ts
@@ -2,22 +2,39 @@ import type { Root, RootContent } from "hast";
 
 export default function rehypeFigCaption() {
 	return (tree: Root) => {
-		for (const node of tree.children) {
+		for (const element of tree.children) {
+			parseChildren(element);
+		}
+	};
+
+	function parseChildren(node: RootContent) {
+		if (!("children" in node)) return;
+
+		for (let index = 0; index < node.children.length; index++) {
+			const element = node.children[index];
+			parseChildren(element);
+
 			// @ts-expect-error - type doesn't exist
 			if (!(node.type === "mdxJsxFlowElement" && node.name === "figure")) {
 				continue;
 			}
 
+			let figCaption = undefined;
+			let figCaptionIndex = 0;
+
 			// @ts-expect-error - type mitigation
 			const children = node.children as RootContent[];
 
-			for (const node of children) {
+			for (let childIndex = 0; childIndex < children.length; childIndex++) {
+				const node = children[childIndex];
 				if (
 					// @ts-expect-error - type doesn't exist
 					!(node.type === "mdxJsxFlowElement" && node.name === "figcaption")
 				) {
 					continue;
 				}
+				figCaption = node;
+				figCaptionIndex = childIndex;
 
 				// @ts-expect-error - I'm in hell
 				const classAttribute = node.attributes.find(
@@ -35,7 +52,33 @@ export default function rehypeFigCaption() {
 				} else {
 					classAttribute.value += ` ${classToAdd}`;
 				}
+
+				// @ts-expect-error - I hate this
+				node.attributes.push({
+					type: "mdxJsxAttribute",
+					name: "id",
+					value: `fig-${index}-${figCaptionIndex}`,
+				});
+			}
+
+			if (figCaption) {
+				// Blindly following WAI-ARIA's specific <figure> formatting
+				// https://www.w3.org/WAI/tutorials/page-structure/content/#figures
+
+				// @ts-expect-error - im in hell
+				node.attributes.push(
+					{
+						type: "mdxJsxAttribute",
+						name: "role",
+						value: "group",
+					},
+					{
+						type: "mdxJsxAttribute",
+						name: "aria-labelledby",
+						value: `fig-${index}-${figCaptionIndex}`,
+					},
+				);
 			}
 		}
-	};
+	}
 }
