619dbff1c2f68e9e43ef32240f75029270615ae4
Fio Cobbs <20820404-fiocobbs@users.noreply.gitlab.com>
2025-05-06 14:19:48 -0500

    chore: formatting
    Attempted to use antfu eslint stylistic, it was a mess with Astro. These files are good though.

--- astro.config.ts
+++ astro.config.ts
@@ -1,24 +1,25 @@
-import AutoImport from "astro-auto-import";
-import { defineConfig } from "astro/config";
-
+/* eslint-disable node/prefer-global/process */
 import cloudflare from "@astrojs/cloudflare";
 import { rehypeHeadingIds } from "@astrojs/markdown-remark";
+
 import mdx from "@astrojs/mdx";
 import netlify from "@astrojs/netlify";
 import svelte from "@astrojs/svelte";
-
 import rehypeEnhancedTables from "@benjc/rehype-enhanced-tables";
 import withToc from "@stefanprobst/rehype-extract-toc";
+
 import withTocExport from "@stefanprobst/rehype-extract-toc/mdx";
+import tailwindcss from "@tailwindcss/vite";
+import AutoImport from "astro-auto-import";
+import { defaultConfig } from "astro-better-image-service";
+import expressiveCode from "astro-expressive-code";
+import { defineConfig } from "astro/config";
 import rehypeCallouts from "rehype-callouts";
+
+import bundlesize from "vite-plugin-bundlesize";
 import rehypeAutolink from "./plugins/rehype-autolink";
 import rehypeFigCaption from "./plugins/rehype-figcaption";
 import remarkReadingTime from "./plugins/remark-reading-time";
-
-import tailwindcss from "@tailwindcss/vite";
-import bundlesize from "vite-plugin-bundlesize";
-import { defaultConfig } from "astro-better-image-service";
-import expressiveCode from "astro-expressive-code";
 
 /* https://docs.netlify.com/configure-builds/environment-variables/#read-only-variables */
 const NETLIFY_PREVIEW_SITE =
