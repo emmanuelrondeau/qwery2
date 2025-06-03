4a1e3ef76111c4b2aced4c1f01d04dcb2d7415ab
Fio Cobbs <fiocobbs@noreply.localhost>
2025-06-02 21:53:40 -0500

    fix: skip to main content a11y
    Skips over the table of contents when using the skip to main button, like docs.astro.build.

--- src/constants.ts
+++ src/constants.ts
@@ -1,3 +1,4 @@
-export const PAGE_TITLE_ID = "_overview";
+export const PAGE_TITLE_ID = "_intro";
+export const MAIN_CONTENT_ID = "main-content";
 export const MAIN_NAV_ID = "main-nav";
 export const BOTTOM_PAGE_SENTINEL_ID = "bottom-page-sentinel";
