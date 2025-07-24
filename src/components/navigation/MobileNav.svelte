4907b57571c64fe9706a0240be2d0ea61a644adf
Fio Cobbs <20820404-fiocobbs@users.noreply.gitlab.com>
2025-07-23 20:07:44 -0500

    feat: mobile navigation
    Needs work but it now exists

---
+++ src/components/navigation/MobileNav.svelte
@@ -0,0 +1,63 @@
+<script lang="ts">
+	// import MobileLink from "./mobile-link.svelte";
+	// import { Button } from "$lib/components/ui/button/index.js";
+	// import { navigation } from "$lib/config/index.js";
+	import { Popover } from "bits-ui";
+	// import ScrollArea from "../scroll-area.svelte";
+	// import { page } from "$app/state";
+	import MobileMenuIcon from "./MobileMenuIcon.svelte";
+
+	interface Props {
+		children?: import("svelte").Snippet;
+	}
+
+	let { children }: Props = $props();
+
+	let myOpen = $state(false);
+	function getOpen() {
+		return myOpen;
+	}
+	function setOpen(newOpen: boolean) {
+		myOpen = newOpen;
+		if (myOpen) {
+			document.body.classList.add("overflow-hidden");
+		} else {
+			document.body.classList.remove("overflow-hidden");
+		}
+	}
+</script>
+
+<Popover.Root bind:open={getOpen, setOpen}>
+	<Popover.Trigger>
+		{#snippet child({ props })}
+			<button
+				{...props}
+				class="ring-focus-outline p-2 text-base outline-hidden select-none focus-visible:rounded-lg focus-visible:ring-2 focus-visible:ring-offset-0 md:hidden [&_svg]:size-7"
+			>
+				<MobileMenuIcon class="label-icon" open={getOpen()} />
+				<span class="sr-only">
+					Toggle menu
+					<noscript>Requires JavaScript</noscript>
+				</span>
+			</button>
+		{/snippet}
+	</Popover.Trigger>
+	<Popover.Portal>
+		<Popover.Content
+			class="bg-background data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=open]:fade-in-0 data-[side=bottom]:slide-in-from-top-2 data-[side=left]:slide-in-from-right-2 data-[side=right]:slide-in-from-left-2 data-[side=top]:slide-in-from-bottom-2 h-(--bits-popover-content-available-height) w-(--bits-popover-content-available-width) origin-(--bits-popover-content-transform-origin)"
+			align="start"
+			side="bottom"
+			alignOffset={-16}
+			sideOffset={14}
+			trapFocus={true}
+		>
+			<div
+				class="mt-2 h-(--bits-popover-content-available-height) max-h-none w-(--bits-popover-content-available-width) max-w-none"
+			>
+				<div class="flex flex-col gap-2 px-4">
+					{@render children?.()}
+				</div>
+			</div>
+		</Popover.Content>
+	</Popover.Portal>
+</Popover.Root>
