59e270646d2084547e49a379299b5e6a965343be
Fio Cobbs <20820404-fiocobbs@users.noreply.gitlab.com>
2025-07-25 17:43:11 -0500

    fix: use unique figure ids

--- src/plugins/rehype-figcaption.ts
+++ src/plugins/rehype-figcaption.ts
@@ -1,84 +1,91 @@
-import type { Root, RootContent } from "hast";
+import type { ElementContent, Root, RootContent } from "hast";
 
 export default function rehypeFigCaption() {
 	return (tree: Root) => {
+		let figureCount = 0;
 		for (const element of tree.children) {
-			parseChildren(element);
+			figureCount = parseChildren(element, figureCount);
 		}
 	};
 
-	function parseChildren(node: RootContent) {
-		if (!("children" in node)) return;
-
-		for (let index = 0; index < node.children.length; index++) {
-			const element = node.children[index];
-			parseChildren(element);
-
-			// @ts-expect-error - type doesn't exist
-			if (!(node.type === "mdxJsxFlowElement" && node.name === "figure")) {
-				continue;
-			}
-
-			let figCaption = undefined;
-			let figCaptionIndex = 0;
-
-			// @ts-expect-error - type mitigation
-			const children = node.children as RootContent[];
-
-			for (let childIndex = 0; childIndex < children.length; childIndex++) {
-				const node = children[childIndex];
-				if (
-					// @ts-expect-error - type doesn't exist
-					!(node.type === "mdxJsxFlowElement" && node.name === "figcaption")
-				) {
-					continue;
-				}
-				figCaption = node;
-				figCaptionIndex = childIndex;
-
-				// @ts-expect-error - I'm in hell
-				const classAttribute = node.attributes.find(
-					// @ts-expect-error - I'm in hell
-					(attr) => attr.name === "class",
-				);
-				const classToAdd = "mx-auto text-center italic max-w-[90%]";
-				if (!classAttribute) {
-					// @ts-expect-error - I'm in hell
-					node.attributes.push({
-						type: "mdxJsxAttribute",
-						name: "class",
-						value: classToAdd,
-					});
-				} else {
-					classAttribute.value += ` ${classToAdd}`;
-				}
-
-				// @ts-expect-error - I hate this
-				node.attributes.push({
-					type: "mdxJsxAttribute",
-					name: "id",
-					value: `fig-${index}-${figCaptionIndex}`,
-				});
-			}
-
-			if (figCaption) {
-				// Blindly following WAI-ARIA's specific <figure> formatting
-				// https://www.w3.org/WAI/tutorials/page-structure/content/#figures
-
-				// @ts-expect-error - im in hell
-				node.attributes.push(
-					{
-						type: "mdxJsxAttribute",
-						name: "role",
-						value: "group",
-					},
-					{
-						type: "mdxJsxAttribute",
-						name: "aria-labelledby",
-						value: `fig-${index}-${figCaptionIndex}`,
-					},
-				);
-			}
+	function parseChildren(node: RootContent, figureCount: number) {
+		// Iterate through all children recursively
+		if (!("children" in node)) return figureCount;
+		for (const element of node.children) {
+			figureCount = parseChildren(element, figureCount);
 		}
+
+		// Retrieve figure element if it exists
+		if (
+			// @ts-expect-error - type doesn't exist
+			!(node.type === "mdxJsxFlowElement" && node.name === "figure")
+		) {
+			return figureCount;
+		}
+		const figureElement = node as ElementContent;
+
+		// Attempts to grab the element's children to scan for a figcaption
+		// (Assumes only one exists)
+		const children = "children" in figureElement ? figureElement.children : [];
+		const figCaptionIndex = children.findLastIndex(
+			(c) =>
+				// @ts-expect-error - type doesn't exist
+				c.type === "mdxJsxFlowElement" && c.name === "figcaption",
+		);
+
+		// Don't continue if there's no figure with figcaption element
+		if (figCaptionIndex === -1) return figureCount;
+		const figCaption = children[figCaptionIndex];
+		figureCount++;
+
+		// Start applying classes into figcaption for styling
+		// @ts-expect-error - I'm in hell
+		const classAttribute = figCaption.attributes?.find(
+			// @ts-expect-error - I'm in hell
+			(attr) => attr.name === "class",
+		);
+		const classToAdd = "mx-auto text-center italic";
+		const classWidth = "max-w-[90%]";
+		if (!classAttribute) {
+			// @ts-expect-error - I'm in hell
+			figCaption.attributes.push({
+				type: "mdxJsxAttribute",
+				name: "class",
+				value: `${classToAdd} ${classWidth}`,
+			});
+		} else {
+			// Filters for "max-w-", to allow any explicit declaration priority
+			const newClass = classAttribute.value.includes("max-w-")
+				? classToAdd
+				: `${classToAdd} ${classWidth}`;
+			classAttribute.value += ` ${newClass}`;
+		}
+
+		// Adds ID required for aria-labelledby
+		const id = `fig-${figureCount}`;
+		// @ts-expect-error - I hate this
+		figCaption.attributes.push({
+			type: "mdxJsxAttribute",
+			name: "id",
+			value: id,
+		});
+
+		// Blindly following WAI-ARIA's specific <figure> formatting
+		// https://www.w3.org/WAI/tutorials/page-structure/content/#figures
+		// @ts-expect-error - perhaps, this is hell
+		figureElement.attributes.push(
+			{
+				type: "mdxJsxAttribute",
+				name: "aria-labelledby",
+				value: id,
+			},
+			{
+				type: "mdxJsxAttribute",
+				name: "role",
+				value: "group",
+			},
+		);
+
+		return figureCount;
 	}
 }
