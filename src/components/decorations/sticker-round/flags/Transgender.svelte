a068a6a12e4da6394db718e22d0ce8e049e993fc
Fio Cobbs <20820404-fiocobbs@users.noreply.gitlab.com>
2025-08-02 23:44:23 -0500

    fix: trans sticker pink color

--- src/components/decorations/sticker-round/flags/Transgender.svelte
+++ src/components/decorations/sticker-round/flags/Transgender.svelte
@@ -1,5 +1,5 @@
 <div class="h-[120%] w-full bg-sky-300"></div>
-<div class="h-full w-full bg-pink-300"></div>
+<div class="h-full w-full bg-pink-200"></div>
 <div class="h-full w-full bg-white"></div>
-<div class="h-full w-full bg-pink-300"></div>
+<div class="h-full w-full bg-pink-200"></div>
 <div class="h-[120%] w-full bg-sky-300"></div>
